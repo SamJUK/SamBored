4dfa0fcd1bf5da4737f955e2a4436e4cbd0eaad4
Sam James <nintendosamhd@gmail.com>
2016-09-02 17:47:17 +0100

    Dark/Light Theme | Mobile Optimization

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
     <link href="https://fonts.googleapis.com/css?family=Catamaran" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
-    <!--<link href="light.css" rel="stylesheet">-->
-    <!--<link href="dark.css" rel="stylesheet">-->
+    <link href="dark.css" rel="stylesheet">
   </head>
   <body>
     <!--<div id="spinner">
@@ -20,8 +19,14 @@
       <div class="sk-cube sk-cube9"></div>
     </div>-->
     <div id="container">
-      <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
-      <h1>Sam Bored</h1>
+      <a href="https://github.com/you"><img id="gitribbon" style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+      <div id="titleContainer">
+        <select onChange="themeChange(this)">
+          <option value="light">Light</option>
+          <option value="dark" selected>Dark</option>
+        </select>
+        <h1>Sam Bored</h1>
+      </div>
       <span id="volLabel">Volume:</span>
       <div id="VolumeSlider"></div>
       <div id="SoundSection">
